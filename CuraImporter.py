e08ca47497fce3dc8d86a37bf4a5c5e3fa289b41
GoodStuff11 <34872749+GoodStuff11@users.noreply.github.com>
2019-01-15 10:27:12 -0500

    Update CuraImporter.py

--- CuraImporter.py
+++ CuraImporter.py
@@ -13,7 +13,7 @@ class Window(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         master.title("Cura Importer")
-        self.windows = [self.window0, self.window1]
+        self.windows = [self.window0, self.window1, self.window2]
 
         # get address of cura and the latest cura version you have
         self.cura_address = str(pathlib.Path.home()).replace('\\', '/') + '/AppData/Roaming/cura'
@@ -40,11 +40,7 @@ class Window(tk.Frame):
         self.cura_dir = self.cura_address + '/' + self.radiobuttons_var0.get()
         self.cura_profile_dir = self.cura_dir + self.profile_folder(self.cura_dir)
 
-        def define_cura():
-            self.cura_dir = self.cura_address + '/' + self.radiobuttons_var0.get()
-            self.next_window()
-
-        self.next_button0 = ttk.Button(self, text='Next', command=define_cura)
+        self.next_button0 = ttk.Button(self, text='Next', command=self.next_window)
         self.skipped = False
         if len(versions) == 1:
             self.skipped = True
@@ -54,11 +50,24 @@ class Window(tk.Frame):
         self.grid(column=0, row=0)
 
         # window 1
-        self.check_box1 = []
-        self.upperLabel1 = ttk.Label(self, text='Select the exported folders which you want to import to Cura.\n'
-                                                'Your folder must be in the same folder as this executable for it to appear.')
-        self.import_button1 = ttk.Button(self, text='Import', command=self.ignore, state=tk.DISABLED)
+        self.lowerLabel1 = ttk.Label(self, text='')
+        self.upperLabel1 = ttk.Label(self, text='Please input the directory where you want to import,\n'
+                                                'typing in nothing will import from the folder of the executable')
+        self.folder_dir = tk.StringVar()
+        self.VARIABLES.append(self.folder_dir)
+        self.entry1 = ttk.Entry(self, textvariable=self.folder_dir)
+        self.next_button1 = ttk.Button(self, text='Next')
         self.back_button1 = ttk.Button(self, text='Back', command=self.last_window)
+
+        # window 2
+        self.exported_folders = []  # folder names
+        self.folders_check = []  # variables of folder checkboxes
+
+        self.check_box2 = []
+        self.upperLabel2 = ttk.Label(self, text='Select the exported folders which you want to import to Cura.\n'
+                                                'Your folder must be in the same folder as the one listed \nbefore to appear.')
+        self.import_button2 = ttk.Button(self, text='Import', command=self.ignore, state=tk.DISABLED)
+        self.back_button2 = ttk.Button(self, text='Back', command=self.last_window)
         self.update_window()
 
     @staticmethod
@@ -102,9 +111,9 @@ class Window(tk.Frame):
                     pass
 
             # import profiles if the profile folder is in the EXPORTED CuraFiles #N folder
-            if os.path.exists('./' + folder_name + '/profiles'):
+            if os.path.exists(self.folder_dir + '/' + folder_name + '/profiles'):
                 directory_information = []
-                directory = os.listdir('./' + folder_name + '/profiles')
+                directory = os.listdir(self.folder_dir + '/' + folder_name + '/profiles')
                 directory.sort(key=lambda x: -len(x))
                 # consider os.walk
                 # https://stackoverflow.com/questions/18383384/python-copy-files-to-a-new-directory-and-rename-if-file-name-already-exists
@@ -117,11 +126,13 @@ class Window(tk.Frame):
                         # C:/Users/htpc/AppData/Roaming/cura/3.6/quality_change/
 
                         # NAMING CONVENTION CAN USE SOME WORK
-                        while os.path.exists(self.cura_dir + self.profile_folder(self.cura_dir) + '/' + file_name[:-9] + '_' + str(n) + '.inst.cfg'):
+                        while os.path.exists(
+                                self.cura_dir + self.profile_folder(self.cura_dir) + '/' + file_name[:-9] + '_' + str(
+                                    n) + '.inst.cfg'):
                             n += 1
                         new_file_name = file_name[:-9] + '_' + str(n) + '.inst.cfg'
 
-                    with open('./' + folder_name + '/profiles/' + file_name, 'r') as f:
+                    with open(self.folder_dir + '/' + folder_name + '/profiles/' + file_name, 'r') as f:
                         file_lines = f.readlines()
                         name = file_lines[2][7:-1]
 
@@ -134,7 +145,9 @@ class Window(tk.Frame):
                             try:
                                 f = open(self.cura_dir + self.profile_folder(self.cura_dir) + '/' + di, 'r')
                                 cura_lines = f.readlines()
-                                if cura_lines[2][7:-1] == name and dup_count == 1 or cura_lines[2][7:-1] == name + ' #' + str(dup_count) and dup_count != 1:
+                                if cura_lines[2][7:-1] == name and dup_count == 1 or cura_lines[2][
+                                                                                     7:-1] == name + ' #' + str(
+                                    dup_count) and dup_count != 1:
                                     dup_count += 1
                                     duplicates = True
                                     break
@@ -157,9 +170,9 @@ class Window(tk.Frame):
 
                 # add to the file once all information has been received
                 for d in directory_information:
-                    with open('./' + folder_name + '/profiles/' + d[0], 'r') as file:
+                    with open(self.folder_dir + '/' + folder_name + '/profiles/' + d[0], 'r') as file:
                         lines = file.readlines()
-                    shutil.copy('./' + folder_name + '/profiles/' + d[0],
+                    shutil.copy(self.folder_dir + '/' + folder_name + '/profiles/' + d[0],
                                 self.cura_dir + self.profile_folder(self.cura_dir) + '/' + d[1])
 
                     # modify the file
@@ -177,16 +190,18 @@ class Window(tk.Frame):
 
             # ---------------------------------------------------------
             # import profiles if the materials folder is in the EXPORTED CuraFiles #N folder
-            if os.path.exists('./' + folder_name + '/materials'):
-                directory = os.listdir('./' + folder_name + '/materials')
+            if os.path.exists(self.folder_dir + '/' + folder_name + '/materials'):
+                directory = os.listdir(self.folder_dir + '/' + folder_name + '/materials')
                 for file_name in directory:
                     dup_count = 0
                     if os.path.exists(self.cura_dir + '/materials/' + file_name):
-                        while os.path.exists(self.cura_dir + '/materials/' + file_name[:-17] + str(dup_count) + '.xml.fdm_material'):
+                        while os.path.exists(
+                                self.cura_dir + '/materials/' + file_name[:-17] + str(dup_count) + '.xml.fdm_material'):
                             dup_count += 1
 
-                    shutil.copy('./' + folder_name + '/materials/' + file_name[:-17] + '.xml.fdm_material',
-                                self.cura_dir + '/materials/' + file_name[:-17] + str(dup_count) + '.xml.fdm_material')
+                    shutil.copy(
+                        self.folder_dir + '/' + folder_name + '/materials/' + file_name[:-17] + '.xml.fdm_material',
+                        self.cura_dir + '/materials/' + file_name[:-17] + str(dup_count) + '.xml.fdm_material')
 
         self.master.destroy()
 
@@ -240,12 +255,48 @@ class Window(tk.Frame):
         for i in range(len(self.radiobuttons0)):
             self.radiobuttons0[i].grid(row=i + 1, column=0, sticky=tk.W)
 
+        def callback(*arg):
+            self.cura_dir = self.cura_address + '/' + self.radiobuttons_var0.get()
+
+        self.radiobuttons_var0.trace('w', callback)
+
     def window1(self):
+        # asks for which folder to put exported files into
+        def check_dir():
+            s = self.folder_dir.get().replace('\\', '/')
+            if os.path.exists(s):
+                self.next_window()
+            elif s == '':
+                self.folder_dir.set('.')
+                self.next_window()
+            else:
+                self.lowerLabel1.config(text='Cannot find this directory.', foreground='red')
+
+        def callback(*arg):
+            if len(self.exported_folders) != 0:
+                self.check_box2 = []
+
+                self.folders_check = []
+                self.exported_folders = []
+
+        self.upperLabel1.grid(row=0, column=0)
+        self.entry1.grid(row=1, column=0, sticky=tk.W)
+        self.entry1.config(width=40)
+        self.lowerLabel1.grid(row=2, column=0)
+        self.lowerLabel1.config(text='')
+        self.next_button1.grid(row=3, column=0, sticky=tk.E)
+        self.back_button1.grid(row=3, column=0, sticky=tk.W)
+        self.next_button1.config(command=check_dir)
+
+        self.folder_dir.trace('w', callback)
+
+    def window2(self):
         # get names of folders that were exported from CuraExporter
         self.exported_folders = []
-        directory = os.listdir('./')
+        directory = os.listdir(self.folder_dir.get())
         for d in directory:
-            if os.path.exists('./' + d + '/profiles') or os.path.exists('./' + d + '/materials'):
+            if os.path.exists(self.folder_dir.get() + '/' + d + '/profiles') or os.path.exists(
+                    self.folder_dir.get() + '/' + d + '/materials'):
                 self.exported_folders.append(d)
         folders_check = [tk.IntVar() for i in self.exported_folders]
         self.VARIABLES += folders_check
@@ -253,20 +304,20 @@ class Window(tk.Frame):
         # create check boxes
         for i in range(len(self.exported_folders)):
             check = ttk.Checkbutton(self, variable=folders_check[i], text=self.exported_folders[i])
-            self.check_box1.append(check)
+            self.check_box2.append(check)
 
-        self.upperLabel1.grid(row=0, column=0, columnspan=2, sticky=tk.W)
+        self.upperLabel2.grid(row=0, column=0, columnspan=2, sticky=tk.W)
         for i in range(len(self.exported_folders)):
-            self.check_box1[i].grid(row=1 + i, column=0, sticky=tk.W)
-        self.import_button1.grid(row=1 + len(self.exported_folders), column=1, sticky=tk.E)
+            self.check_box2[i].grid(row=1 + i, column=0, sticky=tk.W)
+        self.import_button2.grid(row=1 + len(self.exported_folders), column=1, sticky=tk.E)
         if not self.skipped:
-            self.back_button1.grid(row=1 + len(self.exported_folders), column=0, sticky=tk.W)
+            self.back_button2.grid(row=1 + len(self.exported_folders), column=0, sticky=tk.W)
 
         def callback(*arg):
             if all(i.get() == 0 for i in folders_check):
-                self.import_button1.config(state=tk.DISABLED, command=self.ignore)
+                self.import_button2.config(state=tk.DISABLED, command=self.ignore)
             else:
-                self.import_button1.config(state=tk.NORMAL, command=self.run)
+                self.import_button2.config(state=tk.NORMAL, command=self.run)
 
         for i in folders_check:
             i.trace('w', callback)
